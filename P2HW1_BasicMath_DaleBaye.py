c8fede39abca774e372b8a0b8c865c3a8edefe6d
bayed8447 <61768648+bayed8447@users.noreply.github.com>
2020-03-03 23:53:00 -0500

    Add files via upload
    2020-03-03
    Uploading P2HW1 - Basic Match Assignment

---
+++ P2HW1_BasicMath_DaleBaye.py
@@ -0,0 +1,34 @@
+# This program performs basic math with Python.
+# It requests two numbers from the user and
+# performs addition and multiplication for them.
+# 
+# February 25th, 2020
+# CTI-110 P2HW1 - Basic Math
+# Dale Baye
+# 
+
+
+# Welcome user with brief explanation so they have an idea what to expect.
+print('Hello, I am here to teach you some Basic Math today!')
+
+# Get the first number value from the user.
+inp_num1 = int( input('Enter a number please: '))
+
+# Get the second number value from the user.
+inp_num2 = int( input('Enter another number please: '))
+
+# Add inp_num1 and inp_num2 to get sum.
+sum_1 = inp_num1 + inp_num2
+# Display the sum of inp_num1 and inp_num2.
+print('Adding your numbers will give you', sum_1)
+
+# Multiply inp_num1 and inp_num2 to get product.
+prod_1 = inp_num1 * inp_num2
+# Display the product of number1 and number2.
+print('Multiplying your numbers will give you', prod_1)
+
+
+
+
+
+
